3a8b77be1b3ef928e1e0284b50db605ef50018d4
cangqiong <cang19910623@163.com>
2017-10-22 23:40:21 +0800

    修改conmon文件

--- src/common.js
+++ src/common.js
@@ -1,5 +1,4 @@
 let serverUrl= 'http://www.xiaocanghai.com:8081';
-let serverUrl= 'http://www.xiaocanghai.com:8081';
 let common = {
   serverUrl: 'http://www.xiaocanghai.com:8081',
   loginUrl: 'http://www.xiaocanghai.com:8081' + '/login/auth'
